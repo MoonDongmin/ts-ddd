69a3219f6dbde0b7f22494785b8cd1543f487261
MoonDongmin <20191836@o365.hanbat.ac.kr>
2025-10-05 14:54:06 +0900

    Member 도메인 로직 개발

--- src/domain/member.ts
+++ src/domain/member.ts
@@ -1,4 +1,5 @@
 import {MemberStatus} from "@/domain/member-status";
+import assert         from "node:assert";
 
 export class Member {
     email: string;
@@ -10,6 +11,14 @@ export class Member {
     status: MemberStatus;
 
     constructor(email: string, nickname: string, passwordHash: string) {
+        if (!email) {
+            throw new Error("Email cannot be null or empty");
+        }
+
+        if (!nickname) {
+            throw new Error("Nickname cannot be null or empty");
+        }
+
         this.email = email;
         this.nickname = nickname;
         this.passwordHash = passwordHash;
@@ -30,6 +39,18 @@ export class Member {
 
     get getStatus(): MemberStatus {
         return this.status;
+    }
+
+    public activate(): void {
+        assert(this.status === MemberStatus.PENDING, "PENDING 상태가 아닙니다.");
+
+        this.status = MemberStatus.ACTIVE;
+    }
+
+    public deactivate(): void {
+        assert(this.status === MemberStatus.ACTIVE, "ACTIVE 상태가 아닙니다.");
+
+        this.status = MemberStatus.DEACTIVATED;
     }
 }
 
--- test/domain/member.spec.ts
+++ test/domain/member.spec.ts
@@ -7,4 +7,45 @@ describe("Member Test", () => {
 
         expect(member.getStatus).toEqual(MemberStatus.PENDING);
     });
+
+    it("constructorNullCheck", () => {
+        expect(() => new Member(null, "Dongmin", "secret"))
+            .toThrow();
+    });
+
+    it("activate", () => {
+        const member: Member = new Member("dongmin@naver.com", "Doming", "secret");
+
+        member.activate();
+
+        expect(member.getStatus).toEqual(MemberStatus.ACTIVE);
+    });
+
+    it("activateFail", () => {
+        const member: Member = new Member("dongmin@naver.com", "Doming", "secret");
+
+        member.activate();
+
+        expect(() => member.activate()).toThrow();
+    });
+
+    it("deactivate", () => {
+        const member: Member = new Member("dongmin@naver.com", "Doming", "secret");
+        member.activate();
+
+        member.deactivate();
+
+        expect(member.getStatus).toEqual(MemberStatus.DEACTIVATED);
+    });
+
+    it("deactivateFail", () => {
+        const member: Member = new Member("dongmin@naver.com", "Doming", "secret");
+
+        expect(() => member.deactivate()).toThrow();
+
+        member.activate();
+        member.deactivate();
+
+        expect(() => member.deactivate()).toThrow();
+    });
 });
